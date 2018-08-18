8f6e5382bcb54dff3602b77f4fb4a1ac6a46fd45
chouyua <logsoi@126.com>
2018-08-18 22:50:02 +0800

    modify

---
+++ src/test/java/z/learn/StringExampleTest.java
@@ -0,0 +1,40 @@
+package z.learn;
+
+import org.junit.Test;
+import org.junit.Before;
+
+/**
+ * StringExample Tester.
+ *
+ * @author chouyua
+ * @version 1.0
+ * @since <pre>Aug 18, 2018</pre>
+ */
+public class StringExampleTest {
+
+    @Before
+    public void before() throws Exception {
+    }
+
+    /**
+     * Method: example()
+     */
+    @Test
+    public void testExample() throws Exception {
+        example.example();
+        example.test1();
+        example.test2();
+        example.test2();
+        example.test3();
+        example.test4();
+        example.test5();
+        example.test6();
+        example.test7();
+        example.test8();
+        example.test9();
+        example.test10();
+    }
+
+
+    private StringExample example = new StringExample();
+} 
