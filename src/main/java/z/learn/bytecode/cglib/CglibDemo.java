3188b0005e6d1459d55088290479a1d7c8fff1a1
chouyua <logsoi@126.com>
2018-09-26 23:18:50 +0800

    modify

--- src/main/java/z/learn/bytecode/cglib/CglibDemo.java
+++ src/main/java/z/learn/bytecode/cglib/CglibDemo.java
@@ -1,17 +1,9 @@
 package z.learn.bytecode.cglib;
 
-import net.sf.cglib.proxy.Enhancer;
-
 public class CglibDemo {
 
     public static void main(String[] args) {
-        Interceptor interceptor = new Interceptor();
-        Enhancer enhancer = new Enhancer();
-        enhancer.setSuperclass(BizClass.class);
-        enhancer.setCallback(interceptor);
-        enhancer.setCallbackFilter(new CallFilter());
-
-        BizClass bizClass = (BizClass) enhancer.create();
+        BizClass bizClass = Interceptor.newInstance(BizClass.class);
         bizClass.sayHello("OK.");
     }
 }
--- src/main/java/z/learn/bytecode/cglib/Interceptor.java
+++ src/main/java/z/learn/bytecode/cglib/Interceptor.java
@@ -1,16 +1,42 @@
 package z.learn.bytecode.cglib;
 
+import net.sf.cglib.proxy.Enhancer;
 import net.sf.cglib.proxy.MethodInterceptor;
 import net.sf.cglib.proxy.MethodProxy;
 
 import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Interceptor implements MethodInterceptor {
+
+    private Interceptor() {
+    }
 
     @Override
     public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
         System.out.println("entered.. interceptor");
         Object obj = methodProxy.invokeSuper(o, objects);
         return obj;
+    }
+
+    private static Map<Class<?>, Object> proxyCache = new HashMap<>();
+
+    public static <T> T newInstance(Class<T> clazz) {
+        Object obj;
+        if (null == (obj = proxyCache.get(clazz))) {
+            synchronized (proxyCache) {
+                if (null == (obj = proxyCache.get(clazz))) {
+                    Interceptor interceptor = new Interceptor();
+                    Enhancer enhancer = new Enhancer();
+                    enhancer.setSuperclass(clazz);
+                    enhancer.setCallback(interceptor);
+                    enhancer.setCallbackFilter(new CallFilter());
+                    obj = enhancer.create();
+                    proxyCache.put(clazz, obj);
+                }
+            }
+        }
+        return clazz.cast(obj);
     }
 }
