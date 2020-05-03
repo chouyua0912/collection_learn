99142707620f77cebd91e8949d9102f5bd35eeb2
chouyua <logsoi@126.com>
2020-05-03 21:06:27 +0800

    modify

--- src/main/java/z/learn/lang/SwitchDemo.java
+++ src/main/java/z/learn/lang/SwitchDemo.java
@@ -4,6 +4,7 @@ package z.learn.lang;
  * 没有匹配的时候会执行default，而且会顺序往下fall-through
  * default在前面的时候，匹配到了就不会执行default了
  * default在后面，匹配到了如果不break或者return，还会fall-through
+ * 放在中间跟放在前面一样，没有命中时候fall-through，命中了不会执行default
  * -----start--------
  * Case 1
  * Case 2
@@ -19,6 +20,8 @@ package z.learn.lang;
  * Case 3
  * default
  * -------end--------
+ * default
+ * Case 3
  */
 public class SwitchDemo {
 
@@ -45,6 +48,18 @@ public class SwitchDemo {
                 System.out.println("default");
         }
         System.out.println("-------end--------");
+        var = 4;
+        switch (var) {
+            case 1:
+                System.out.println("Case 1");
+            case 2:
+                System.out.println("Case 2");
+                break;
+            default:
+                System.out.println("default");
+            case 3:
+                System.out.println("Case 3");
+        }
     }
 
     private static void switchDemo(int var) {
